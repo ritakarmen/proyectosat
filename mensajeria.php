b39f786b4f806fe0d9fb2d5da926a0e167a2e84a
ritakarmen <ritakarmen@gmail.com>
2016-08-12 14:53:44 -0500

    prueba

---
+++ mensajeria.php
@@ -0,0 +1,23 @@
+<?php
+	if(isset($_POST['nombre'])&&($_POST['email']!='')&&($_POST['celular']!='')&&($_POST['mensaje']!='')){
+		$nombre=$_POST['nombre'];
+		$email=$_POST['email'];
+		$celular=$_POST['celular'];
+		$mensaje=$_POST['mensaje'];
+		//contenido del mensaje
+		$titulo = "Mensaje de la web";
+		$contenido = 
+		'
+
+		';
+	}
+
+?>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>Haz recibido un mensaje de la web Liberate del SAT </h1>
+</body>
+</html>
